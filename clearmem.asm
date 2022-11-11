3fb63cd846f0d4521a1bcab93955f443817c1151
ergz <emnlrodriguez@gmail.com>
2022-11-10 20:46:44 -0800

    add headers

--- clearmem.asm
+++ clearmem.asm
@@ -1,6 +1,11 @@
 ; iNES Header
 
 .segment "HEADER"
-.org $7FF0              ;; start at address $7FF0
-.byte $4E,$45,$53,$1A   ;; 4 bytes with character 'N', 'E', 'S', '\n'
-.byte $02
+.org $7FF0              ; start at address $7FF0
+.byte $4E,$45,$53,$1A   ; 4 bytes with character 'N', 'E', 'S', '\n'
+.byte $02               ; 2x 16kb of program code
+.byte $01               ; 1x 8kb of character rom
+.byte %00000000         ; horiz mirror, no battery, no mapper
+.byte %00000000         ; all disabled
+
+
