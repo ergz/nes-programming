d368986ac1d07f15c385ecdf21192853738433a9
ergz <emnlrodriguez@gmail.com>
2022-11-10 21:08:12 -0800

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+to get the assembler go here [https://cc65.github.io/](https://cc65.github.io/)
+
+to get the emulator go here [https://fceux.com/web/download.html](https://fceux.com/web/download.html)
--- clearmem.asm
+++ clearmem.asm
@@ -19,7 +19,16 @@
 RESET:
     sei                     ; disable all interrupts
     cld                     ; clear the decimal mode flag
-    
+    ldx #$FF
+    txs                     ; init the stack pointer at the bottom of the stack
+
+    lda #0
+    ldx #$FF 
+MemLoop:                    ; loop from $FF until the zero flag is enabled
+    sta $0,x                ; store that value in A into the mem position $0+x <- where x is register changing memory
+    dex
+    bne MemLoop
+
 NMI:
     rti
 
