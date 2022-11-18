3f52e25b02806284a9d04836e587a70469b05e56
ergz <emnlrodriguez@gmail.com>
2022-11-17 22:19:37 -0800

    adds ppu warm up loops

--- build.bat
+++ build.bat
@@ -2,11 +2,11 @@
 echo ~~~~ starting build for %1 ~~~~
 
 REM assemble the asm file
-echo ...assembling...
-ca65 -v %1.asm -o %1.o
+echo assembling...
+ca65 %1.asm -o %1.o
 
 REM build the rom for nes
-echo ...linking...
-ld65 -v -C nes.cfg %1.o -o %1.nes
+echo linking...
+ld65 -C nes.cfg %1.o -o %1.nes
 
 echo ~~~~ completed ~~~~
--- helloppu.asm
+++ helloppu.asm
@@ -17,9 +17,19 @@
 RESET:
     sei                     ; disable all interrupts
     cld                     ; clear the decimal mode flag
+    ldx #$40
+    stx $4017               ; disable the API interrupt
     ldx #$FF
+    txs                     ; start stack pointer at $FF
     inx                     ; to wrap around from $FF -> $00
-    txs
+    stx $2000               ; disable NMI by setting to 0
+    stx $2001               ; disable rendering by setting to 0
+    stx $4010               ; disable DMC by setting to 0
+
+WaitVBlank1:                ; loop until bit 7 of the APU register
+    bit $2002
+    bpl WaitVBlank1
+
     txa                     ; A = 0
 
 ; The strategy for the clearing 2kb of RAM when we have only 
@@ -38,12 +48,17 @@ ClearRam:
     inx
     bne ClearRam            ; by the end of this loop we will have cleared mem $0000 - $07FF as needed
 
+WaitVBlank2:                ; loop until bit 7 of the APU register
+    bit $2002
+    bpl WaitVBlank2
+
+
 Main:
-    ldx #$3F
-    stx $2006
-    ldx #$00
-    stx $2006
-    lda #$2A
+    ldx #$3F                ; load into X the value $3F
+    stx $2006               ; store the current value of X ($3F) into the address $2006
+    ldx #$00                ; load into X the value $00
+    stx $2006               ; store the current value in X into the address $2006
+    lda #$2B
     sta $2007
     lda #%00011110
     sta $2001
